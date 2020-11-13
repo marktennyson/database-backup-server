0ab512613c9dc7d3a9a2d08873d0b18f3de01433
Mark Tennyson <46404058+marktennyson@users.noreply.github.com>
2020-11-13 13:14:26 +0530

    some modifications

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,10 @@ app.get( "/", ( req: any, res:any ) => {
 cron.schedule('45 23 * * *', ():any => {
     const dbName: string = process.env.DB_NAME;
     const dbPath: string = process.env.DB_PATH;
-    const datestamps: string = moment().subtract(10, 'days').calendar();
+    const datestamps: string = moment().format("DD_MM_YYYY")
     const dbFileName: string = process.env.DB_FILE_NAME;
-    const sqlFileName: string = dbPath+datestamps.replace(/\//g, "_")+`${dbFileName}`;
+    const sqlFileName: string = dbPath+datestamps+`${dbFileName}`;
+    console.log(sqlFileName);
     if (shell.exec(`mysqldump ${dbName} > ${sqlFileName}`).code !== 0) {
         shell.echo(`database backup cron job failed for : ${datestamps}`);
         shell.exit(1);
@@ -27,10 +28,6 @@ cron.schedule('45 23 * * *', ():any => {
         shell.echo(`Database backup complete for : ${datestamps}`);
       }
 });
-
-
-
-
 
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
